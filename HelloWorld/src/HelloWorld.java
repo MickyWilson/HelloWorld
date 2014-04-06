ae4d000d061d4fcd6a6a72b421a055c427914a61
Mike Wilson <mike.m.wilson@lmco.com>
2014-04-06 19:58:17 +0100

    Buffered file added.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloWorld
 {
 
@@ -6,4 +11,43 @@ public class HelloWorld
         System.out.println("Hello World");
     }
 
+
+    public void run() {
+        
+        String csvFile = "/Users/mkyong/Downloads/GeoIPCountryWhois.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+     
+        try {
+     
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+     
+                    // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+     
+                System.out.println("Country [code= " + country[4] 
+                                     + " , name=" + country[5] + "]");
+     
+            }
+     
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+     
+        System.out.println("Done");
+      }
+    
+    
 }
