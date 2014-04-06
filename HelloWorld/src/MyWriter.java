32b16ff7faa9ad8a6e912868062f7fd71434ccde
Mike Wilson <mike.m.wilson@lmco.com>
2014-04-06 21:16:10 +0100

    Added timing

---
+++ HelloWorld/src/MyWriter.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+
+public class MyWriter
+{
+
+    static String filename = "C:\\temp\\log.txt";
+    public static void main(String[] args)
+    {
+        long startTime = System.currentTimeMillis();
+
+        run();
+        
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+    }
+   
+    static void run()
+    {
+        final long c = 1000000;
+        
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File(filename))))
+        {
+            for(long i = 0; i != c; ++i)
+                writer.write("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+}
+
